377bc0d8167b4417a143c51857cceeebb8a0201d
wass3 <unlessbbx03@gmail.com>
2023-05-15 12:05:34 +0200

    Created "Test2" -- Input: "fly" Output: "There was an old lady who swallowed a fly.\n" + "I don't know why she swallowed a fly - perhaps she'll die!\n" ;

--- SongTDD/src/TDDSong.java
+++ SongTDD/src/TDDSong.java
@@ -4,6 +4,11 @@ public class TDDSong {
 
 
     public static String cancion(List<String> listaAnimales) {
+        if (listaAnimales.size() == 1) {
+            return "There was an old lady who swallowed a " + listaAnimales.get(0) + ".\n" +
+                    "I don't know why she swallowed a " + listaAnimales.get(0) + " - perhaps she'll die!\n" ;
+        }
+        System.out.println("No llega");
         return "";
     }
 }
--- SongTDD/src/TDDSongTest.java
+++ SongTDD/src/TDDSongTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,9 +10,18 @@ class TDDSongTest {
 
     @Test
     public void cancionTest1() {
-        List<String> listaAnimales = Arrays.asList("");
+        List<String> listaAnimales = new ArrayList<>();
         String cadenaReal = TDDSong.cancion(listaAnimales);
         String cadenaEsperada = "";
+        assertEquals(cadenaEsperada, cadenaReal);
+    }
+
+    @Test
+    public void cancionTest2() {
+        List<String> listaAnimales = Arrays.asList("fly");
+        String cadenaReal = TDDSong.cancion(listaAnimales);
+        String cadenaEsperada = "There was an old lady who swallowed a fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" ;
         assertEquals(cadenaEsperada, cadenaReal);
     }
 
